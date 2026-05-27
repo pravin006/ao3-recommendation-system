09e24b1501d4bb5dd3a43274c448b4b283632af4
pravin006 <pravin201197@gmail.com>
2026-05-27 18:39:24 +0800

    add scripts to unpause and run dags for retrieving new works, adding my bookmarks and running the recommender

---
+++ scripts/add_my_bookmarks.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DAG_ID="ao3_collect_and_enrich"
+
+MODE="manual"
+
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: bash $0 link1 link2 link3 ..."
+    exit 1
+fi
+
+first=true
+    
+links="["
+
+for link in "$@"; do
+    if [ "$first" = true ]; then
+        links+="\"$link\""
+        first=false
+    else
+        links+=",\"$link\""
+    fi
+done
+
+links+="]"
+
+docker compose exec airflow-scheduler airflow dags unpause "$DAG_ID"
+docker compose exec airflow-scheduler airflow dags trigger "$DAG_ID" --conf "{\"source_mode\": \"${MODE}\", \"manual_work_links\": ${links}}"
